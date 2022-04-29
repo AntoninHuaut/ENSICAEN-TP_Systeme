b2eb528b6188625126536fcf7f9fbd1644216fa7
Antonin Huaut <antonin.huaut@gmail.com>
2022-04-29 12:02:58 +0200

    TP4/Code par défaut

---
+++ TP4/include/matrix.h
@@ -0,0 +1,35 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex
+ *
+ * Unix System Programming
+ * Chapter "Threads"
+ *
+ * 2016 Alain Lebret (alain.lebret@ensicaen.fr)
+ */
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#define SIZE 1500
+#define PRINT 0
+
+typedef struct matrix {
+  unsigned int rows;
+  unsigned int columns;
+  double** matrix;
+} matrix_t;
+
+/* Mono thread functions */
+void mono_thread(matrix_t* m1, matrix_t* m2);
+matrix_t* product_matrix(matrix_t* m1, matrix_t* m2);
+double product_case(int r, int c, matrix_t* m1, matrix_t* m2);
+
+/* Utils matrix functions */
+void print_matrix(matrix_t* m);
+matrix_t* init_matrix(unsigned int rows, unsigned int columns, int fill);
+void fill_matrix(matrix_t* m);
+double** alloc_matrix(unsigned int rows, unsigned int columns);
+void free_matrix(double** matrix);
+
+#endif /* MATRIX_H */
